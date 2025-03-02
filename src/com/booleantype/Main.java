e86b131267240a02313f118ef7b454580e648bab
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-03-02 23:00:55 +0100

    Add files via upload
    Commiting java program with boolean data type demonstration

---
+++ src/com/booleantype/Main.java
@@ -0,0 +1,31 @@
+package com.booleantype;
+//Java Program to Demonstrate Boolean Primitive DataType
+
+// Class
+public class Main {
+
+    // Main method to run java application
+    public static void main(String[] args) {
+
+        // Boolean data type that can hold one of two values true or false
+        // Creating two variables with boolean type
+
+        boolean a_boolean = true;
+        boolean b_boolean = false;
+
+        // If condition holds
+        if (a_boolean == true){
+
+            // Print statement
+            System.out.println("a_boolean has value true");
+        }
+
+        // If condition holds
+        if (b_boolean == false){
+
+            // Print statements
+            System.out.println("b_boolean has value false");
+        }
+
+    }
+}
